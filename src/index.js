32525fcbb22b87bf71b7ffb7a4db6fb05efff570
Shamdee <shamdee@gmail.com>
2022-01-03 22:34:37 +0200

    fix: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = String(n);
+    (n[0] === "-") ? n = n.slice(1): n
+    let result = '';
+    for (let i = n.length - 1; i >= 0; i--) {
+        result += n[i]
+    }
+    return +result;
 }
